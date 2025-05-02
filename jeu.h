cee40f5099512a6f3a5d2c813ddcd6904b4e0124
Yoll <antoine.ecobichon@gmail.com>
2025-05-02 13:56:34 +0200

    Update jeu.h
    j'ai remplacé pickaxe (une pioche qui sert casser des cailloux par "pile" qui represente une pile de carte)

--- jeu.h
+++ jeu.h
@@ -3,13 +3,13 @@
 typedef struct{
     Player * players; //joueurs maximum 8 et minimum 2
     int nb_players; //nombre de joueurs
-    Card *pickaxe; // pioche
-    int size_pickaxe; //taille de la pioche  
+    Card * pile; // pioche
+    int size_pile; //taille de la pioche  
 }Party;
 
 int nb_players();
 int nb_cards(int play);
-Party build_players(int *pickaxe_size);
-void draw_pile_display(Party p, int * pickaxe_size );
-void display_party(Party p , int *pickaxe_size, int i );
+Party build_players(int *pile_size);
+void draw_pile_display(Party p, int * pile_size );
+void display_party(Party p , int *pile_size, int i );
 void free_party(Party p);
