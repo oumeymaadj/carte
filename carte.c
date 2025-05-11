cbda83d5ae48f45fdcf6aa0ff09ad6b04c0fd836
QuiQUiflex <ethangnass@gmail.com>
2025-05-11 09:33:20 +0100

    traduction carte.c

--- carte.c
+++ carte.c
@@ -1,7 +1,7 @@
 #include"carte.h"
 
 
-int* deck(int d[150]) {
+int* paquet(int d[150]) {
     int compteur = 0;
     int valeur;
     int quantite;
@@ -65,7 +65,7 @@ int* deck(int d[150]) {
             quantite = better_scan_int("Entrez une quantité correcte (1-15, sans dépasser 150 au total)\n");
         }
 
-        // Ajout au deck
+        // Ajout au paquet
         for (int j = 0; j < quantite; j++) {
             d[compteur] = valeur;
             compteur++;
@@ -78,7 +78,7 @@ int* deck(int d[150]) {
 
 
 // Mélange en place un tableau de 150 cartes
-void shuffle(int* d) {
+void melanger(int* d) {
     for (int i = 149; i > 0; i--) {
         int j = rand() % (i + 1); // indice aléatoire entre 0 et i
         // Échange les valeurs d[i] et d[j]
@@ -90,74 +90,74 @@ void shuffle(int* d) {
 
 
 
-Player build_player(int nomb_cards, int *deck,int *start){ //deck: le paquet de cartes , start: pointeur qui contient l'indice d'ou on commence a prendre les cartes en gros on a un paquet de cartes et on distribut les cartes dans l'ordre, ex: le premier a les cartes de 1 a 15 le deuxieme de 15 a 30 du coup start c'est le pointeur de l'indice du debut
-    Player p;
+Joueur construire_joueur(int nombre_cartes, int *paquet,int *commencer){ //paquet: le paquet de cartes , commencer: pointeur qui contient l'indice d'ou on commence a prfinre les cartes en gros on a un paquet de cartes et on distribut les cartes dans l'ordre, ex: le premier a les cartes de 1 a 15 le deuxieme de 15 a 30 du coup commencer c'est le pointeur de l'indice du debut
+    Joueur p;
     int j =0; // indice du paquet du joueurs;
     char n[100];
     better_scan_str("Entrez votre pseudo (50 caracteres max): ", n, 51); // 51 = 50 + '\0'
-    p.name = malloc(sizeof(char)*strlen(n) +1 );// +1 pour /0
-    if(p.name == NULL){
+    p.nom = malloc(sizeof(char)*strlen(n) +1 );// +1 pour /0
+    if(p.nom == NULL){
         printf("erreur allocation dynamique \n"); //on quitte le programme
         exit(1);
     }
-    strcpy(p.name, n); //copie le contenu de la chaîne n (le pseudo entré par l'utilisateur) dans p.name
-    printf("Bienvenue, %s!\n", p.name);
-    p.nb_cards = nomb_cards;
-    p.cards = malloc(sizeof(Card)*p.nb_cards);
-    if (p.cards == NULL) {
+    strcpy(p.nom, n); //copie le contenu de la chaîne n (le pseudo entré par l'utilisateur) dans p.nom
+    printf("Bienvenue, %s!\n", p.nom);
+    p.nb_cartes = nombre_cartes;
+    p.Cartes = malloc(sizeof(Carte)*p.nb_cartes);
+    if (p.Cartes == NULL) {
         printf("erreur allocation dynamique \n");
         exit(2);
     }
-    for(int i= *start; i<p.nb_cards+ *start;i++){
-        p.cards[j].value = deck[i]; // distribution des cartes 
-        p.cards[j].seeable = 0; // mettre toute les cartes invisible
+    for(int i= *commencer; i<p.nb_cartes+ *commencer;i++){
+        p.Cartes[j].valeur = paquet[i]; // distribution des cartes 
+        p.Cartes[j].visible = 0; // mettre toute les cartes invisible
         j++;
 
     }
-    *start += p.nb_cards; // avancement du premier indice pour le prochain joueur
-    p.discard.value = 0;
-    p.discard.seeable = 0;
+    *commencer += p.nb_cartes; // avancement du premier indice pour le prochain joueur
+    p.defausse.valeur = 0;
+    p.defausse.visible = 0;
     p.score = 0;
     return p;
     
 }
 
 
-void display_card(Player p, int end) { // affiche joliment un paquet de cartes d’un joueur
-    printf(" Cartes du joueur: %s\n\n", p.name);
+void affichage_Carte(Joueur p, int fin) { // affiche joliment un paquet de cartes d’un joueur
+    printf(" Cartes du joueur: %s\n\n", p.nom);
 
     // Ligne 1 : Indices
-    for (int i = 0; i < end; i++) {
+    for (int i = 0; i < fin; i++) {
         printf("   (%2d)     ", i);
     }
     printf("\n");
 
     // Ligne 2 : Haut des cartes
-    for (int i = 0; i < end; i++) {
+    for (int i = 0; i < fin; i++) {
         printf("  _______   ");
     }
     printf("\n");
 
     // Ligne 3 : Visuel vide
-    for (int i = 0; i < end; i++) {
+    for (int i = 0; i < fin; i++) {
         printf(" |       |  ");
     }
     printf("\n");
 
     // Ligne 4 : Valeur
-    for (int i = 0; i < end; i++) {
-        if (p.cards[i].seeable == 1){
-            if(p.cards[i].value >= -5 && p.cards[i].value <= 0 ){
-                printf(VERT " |  %3d  |  " FIN_COULEUR, p.cards[i].value);
+    for (int i = 0; i < fin; i++) {
+        if (p.Cartes[i].visible == 1){
+            if(p.Cartes[i].valeur >= -5 && p.Cartes[i].valeur <= 0 ){
+                printf(VERT " |  %3d  |  " FIN_COULEUR, p.Cartes[i].valeur);
             }
-            else if(p.cards[i].value >= 1 && p.cards[i].value <= 5 ){
-                printf(BLEU " |  %3d  |  " FIN_COULEUR, p.cards[i].value);
+            else if(p.Cartes[i].valeur >= 1 && p.Cartes[i].valeur <= 5 ){
+                printf(BLEU " |  %3d  |  " FIN_COULEUR, p.Cartes[i].valeur);
             }
-            else if(p.cards[i].value >= 6 && p.cards[i].value <= 10 ){
-                printf(JAUNE " |  %3d  |  " FIN_COULEUR, p.cards[i].value);
+            else if(p.Cartes[i].valeur >= 6 && p.Cartes[i].valeur <= 10 ){
+                printf(JAUNE " |  %3d  |  " FIN_COULEUR, p.Cartes[i].valeur);
             }
-            else if(p.cards[i].value >= 11 && p.cards[i].value <= 15 ){
-                printf(ROUGE " |  %3d  |  " FIN_COULEUR, p.cards[i].value);
+            else if(p.Cartes[i].valeur >= 11 && p.Cartes[i].valeur <= 15 ){
+                printf(ROUGE " |  %3d  |  " FIN_COULEUR, p.Cartes[i].valeur);
             }
         }
         else{
@@ -167,7 +167,7 @@ void display_card(Player p, int end) { // affiche joliment un paquet de cartes d
     printf("\n");
 
     // Ligne 5 : Bas des cartes
-    for (int i = 0; i < end; i++) {
+    for (int i = 0; i < fin; i++) {
         printf(" |_______|  ");
     }
     printf("\n\n");
@@ -175,8 +175,8 @@ void display_card(Player p, int end) { // affiche joliment un paquet de cartes d
 
 
 
-void display_discard(Player p) { // affiche la défausse du joueur joliment
-    printf(" Défausse du joueur: %s\n\n", p.name);
+void affichage_defausse(Joueur p) { // affiche la défausse du joueur joliment
+    printf(" Défausse du joueur: %s\n\n", p.nom);
 
     // Ligne 1 : Titre centré
     printf("     Défausse    \n");
@@ -188,18 +188,18 @@ void display_discard(Player p) { // affiche la défausse du joueur joliment
     printf(" |       |  \n");
 
     // Ligne 4 : Valeur visible ou cachée
-    if (p.discard.seeable == 1) {
-        if(p.discard.value >= -5 && p.discard.value <= 0 ){
-            printf(VERT " |  %3d  |  " FIN_COULEUR, p.discard.value);
+    if (p.defausse.visible == 1) {
+        if(p.defausse.valeur >= -5 && p.defausse.valeur <= 0 ){
+            printf(VERT " |  %3d  |  " FIN_COULEUR, p.defausse.valeur);
         }
-        else if(p.discard.value >= 1 && p.discard.value <= 5 ){
-            printf(BLEU " |  %3d  |  " FIN_COULEUR, p.discard.value);
+        else if(p.defausse.valeur >= 1 && p.defausse.valeur <= 5 ){
+            printf(BLEU " |  %3d  |  " FIN_COULEUR, p.defausse.valeur);
         }
-        else if(p.discard.value >= 6 && p.discard.value <= 10 ){
-            printf(JAUNE " |  %3d  |  " FIN_COULEUR, p.discard.value);
+        else if(p.defausse.valeur >= 6 && p.defausse.valeur <= 10 ){
+            printf(JAUNE " |  %3d  |  " FIN_COULEUR, p.defausse.valeur);
         }
-        else if(p.discard.value >= 11 && p.discard.value <= 15 ){
-            printf(ROUGE " |  %3d  |  " FIN_COULEUR, p.discard.value);
+        else if(p.defausse.valeur >= 11 && p.defausse.valeur <= 15 ){
+            printf(ROUGE " |  %3d  |  " FIN_COULEUR, p.defausse.valeur);
         }
     } else {
         printf(" |  ??   |  ");
